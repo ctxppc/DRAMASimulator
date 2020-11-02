6124d36efeedfdb0f213a1185e032046b61b75b8
Constantino Tsarouhas <c@tsarouhas.eu>
2020-11-02 20:52:09 +0100

    Partially implements preprocessor.

--- Model/Preprocessor/Preprocessor.swift
+++ Model/Preprocessor/Preprocessor.swift
@@ -2,72 +2,93 @@
 
 import Foundation
 
-/// A value that processes source directives.
-///
-/// A preprocessor transforms a sequence of lexical units.
+/// A value that processes a translation unit into a form that can be parsed into a compilation unit.
 struct Preprocessor {
 	
-	/// Initialises and runs a preprocessor on given source text.
-	init(from text: String) throws {
+	/// Creates a preprocessor for processing given translation unit.
+	init(translationUnit: TranslationUnit) {
+		self.translationUnit = translationUnit
+	}
+	
+	/// The translation unit being processed.
+	private(set) var translationUnit: TranslationUnit
+	
+	/// A Boolean value indicating whether the translation unit is processed into a form that can be parsed into a compilation unit.
+	var processed: Bool {
+		translationUnit.isProcessed
+	}
+	
+	/// The processed macros.
+	var macros: [Macro] = []
+	
+	/// The current scope.
+	var scope: Scope = .global(values: [:])
+	
+	/// A mapping of variables to values in some lexical scope.
+	enum Scope {
+		
+		/// A global scope.
+		case global(values: [String : Int])
+		
+		/// A local scope.
+		indirect case local(values: [String : Int], outerScope: Scope)
+		
+		/// Accesses a variable.
+		subscript (variable: String) -> Int? {
+			
+			get {
+				switch self {
+					
+					case .global(values: let values):
+						return values[variable]
+						
+					case .local(values: let values, outerScope: let outerScope):
+						return values[variable] ?? outerScope[variable]
+				}
+			}
+			
+			set {
+				switch self {
+					
+					case .global(values: var values):
+						values[variable] = newValue
+						self = .global(values: values)
+						
+					case .local(values: var values, outerScope: let outerScope):
+						values[variable] = newValue
+						self = .local(values: values, outerScope: outerScope)
+						
+				}
+			}
+			
+		}
+		
+		/// Enters an inner scope.
+		mutating func enter() {
+			self = .local(values: [:], outerScope: self)
+		}
+		
+		/// Leaves the scope.
+		mutating func leave() {
+			switch self {
+				
+				case .global:
+					preconditionFailure("Cannot leave global scope")
+					
+				case .local(values: _, outerScope: let outerScope):
+					self = outerScope
+					
+			}
+		}
+		
+	}
+	
+	/// The preprocessor's condition state.
+	var conditionState: ConditionState = .zero
+	
+	/// Processes the next lexical unit or directive.
+	mutating func processNext() throws {
 		TODO.unimplemented
 	}
-	
-	/// The source text that is the input to the preprocessor.
-	let input: String
-	
-	/// The range in the input text that has been processed.
-	var processedRange: SourceRange {
-		return input.startIndex..<indexOfFirstUnprocessedCharacter
-	}
-	
-	/// The range in the input text that remains to be processed.
-	var remainingRange: SourceRange {
-		return indexOfFirstUnprocessedCharacter..<input.endIndex
-	}
-	
-	/// The source text that is the output from the preprocessor.
-	var output: String {
-		return substitutions
-			.sorted(by: { $0.range.lowerBound < $1.range.lowerBound })
-			.reduce(into: (output: "", indexOfNextInputCharacter: "".startIndex)) { state, substitution in
-				state.output.append(contentsOf: input[state.indexOfNextInputCharacter..<substitution.range.lowerBound])
-				state.output.append(substitution.newText)
-				state.indexOfNextInputCharacter = substitution.range.upperBound
-			}.output
-	}
-	
-	/// The substitutions performed by the preprocessor.
-	private(set) var substitutions: [Substitution]
-	struct Substitution {
-		
-		/// The range of unprocessed source text being replaced.
-		let range: SourceRange
-		
-		/// The new text replacing the unprocessed substring.
-		let newText: String
-		
-	}
-	
-	/// Adds the unprocessed source text between the first unprocessed character and a given upper bound (not inclusive) to the preprocessor's output.
-	mutating func addPreservedSubstring(upTo upperBound: String.Index) {
-		precondition((indexOfFirstUnprocessedCharacter...input.endIndex).contains(upperBound), "Upper bound not in unprocessed range")
-		indexOfFirstUnprocessedCharacter = upperBound
-	}
-	
-	/// Adds given text to the preprocessor's output and marks it as replacing the unprocessed source text between the first unprocessed character and a given upper bound (not inclusive).
-	mutating func addSubstitution(upTo upperBound: String.Index, newText: String) {
-		precondition((indexOfFirstUnprocessedCharacter...input.endIndex).contains(upperBound), "Upper bound not in unprocessed range")
-		substitutions.append(.init(range: indexOfFirstUnprocessedCharacter..<upperBound, newText: newText))
-		indexOfFirstUnprocessedCharacter = upperBound
-	}
-	
-	/// The index of the first unprocessed character in the unprocessed source text.
-	private var indexOfFirstUnprocessedCharacter: String.Index
-	
-	/// The macros defined in the source.
-	let macros: [Macro]
-	
-	/// The macro invocations in the body of the source (outside of any macro body), in source order.
-	let macroInvocations: [InvocationDirective]
 	
 }
--- Model/Preprocessor/Translation Unit.swift
+++ Model/Preprocessor/Translation Unit.swift
@@ -6,7 +6,7 @@ import Foundation
 ///
 /// A translation unit is created from a sequence of lexical units (from a lexer), then transformed by a preprocessor until it no longer contains directives, and finally used as the input for parsing a compilation unit.
 ///
-/// A translation unit is processed one lexical unit at a time, skipping over lexical units that need no further processing, and is considered preprocessed when all lexical units have been processed.
+/// A translation unit is processed one directive or lexical unit at a time, skipping over lexical units that need no further processing, and is considered preprocessed when all lexical units have been processed.
 struct TranslationUnit {
 	
 	/// Creates a translation unit containing lexical units produced by given lexer.
@@ -33,70 +33,8 @@ struct TranslationUnit {
 	}
 	
 	/// A Boolean value indicating whether the translation unit is processed into a form that can be parsed into a compilation unit.
-	private var isProcessed: Bool {
+	var isProcessed: Bool {
 		unprocessedLexicalUnits.isEmpty
-	}
-	
-	/// The current scope.
-	private var scope: Scope = .global(values: [:])
-	
-	/// A mapping of variables to values in some lexical scope.
-	private enum Scope {
-		
-		/// A global scope.
-		case global(values: [String : Int])
-		
-		/// A local scope.
-		indirect case local(values: [String : Int], outerScope: Scope)
-		
-		/// Accesses a variable.
-		subscript (variable: String) -> Int? {
-			
-			get {
-				switch self {
-					
-					case .global(values: let values):
-					return values[variable]
-						
-					case .local(values: let values, outerScope: let outerScope):
-					return values[variable] ?? outerScope[variable]
-				}
-			}
-			
-			set {
-				switch self {
-					
-					case .global(values: var values):
-					values[variable] = newValue
-					self = .global(values: values)
-						
-					case .local(values: var values, outerScope: let outerScope):
-					values[variable] = newValue
-					self = .local(values: values, outerScope: outerScope)
-					
-				}
-			}
-			
-		}
-		
-		/// Enters an inner scope.
-		mutating func enter() {
-			self = .local(values: [:], outerScope: self)
-		}
-		
-		/// Leaves the scope.
-		mutating func leave() {
-			switch self {
-				
-				case .global:
-				preconditionFailure("Cannot leave global scope")
-					
-				case .local(values: _, outerScope: let outerScope):
-				self = outerScope
-					
-			}
-		}
-		
 	}
 	
 }
